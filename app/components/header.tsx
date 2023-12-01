8c33d23a691e3b4c6d6af822fddafcd8b5189d9a
unknown <ezekuiel100@gmail.com>
2023-12-01 09:53:54 -0300

    New commit

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import { Search, ShoppingBag } from "lucide-react";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex items-center justify-between p-8 ">
+      <div className="flex gap-5 items-center">
+        <Link href="/" className="text-white text-2xl font-extrabold">
+          devstore
+        </Link>
+
+        <form className="flex items-center rounded-full bg-zinc-900 px-5 py-3 ring-2 ring-pink-500">
+          <Search className="w-5 h-5" />
+          <input
+            type="text"
+            placeholder="Buscar produtos"
+            className=" flex-1 bg-transparent outline-none placeholder:text-zinc-500 "
+          />
+        </form>
+      </div>
+
+      <div className="flex gap-5">
+        <div className="flex gap-2">
+          <ShoppingBag />
+          <span>Card (0)</span>
+        </div>
+        <span>Conta</span>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 import Header from "./components/header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      Ola mundo
+      <div className="grid max-h-[860px] ">
+        <Link href="/" className="bg-yellow-500 w-96 h-80">
+          <Image
+            src="/moletom-never-stop-learning.png"
+            alt=""
+            width={860}
+            height={860}
+            quality={100}
+          ></Image>
+        </Link>
+      </div>
     </main>
   );
 }
